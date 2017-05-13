55cc17a5b22d1a8e9370e61e3c0ab930b413c81e
Luke McNulty <luke.mcnulty2@gmail.com>
2017-05-13 01:35:46 +0100

    Rect Canvas is now added to DOM... Clearing of canvas during redraw() now works remove() now removes the canvas from the DOM rather than clears it

--- src/selectingRectangle.ts
+++ src/selectingRectangle.ts
@@ -9,16 +9,15 @@ export class Rect {
     private dimensions: [number, number, number, number]; // x, y, w, h
     private widthIncreasing: boolean;
     private heightIncreasing: boolean;
-    private rectStyler: (CanvasRenderingContext2D) => void;
 
-    constructor(canvas: HTMLCanvasElement, anchorPoint: [number, number], rectStyler?: (CanvasRenderingContext2D) => void) {
+    constructor(canvas: HTMLCanvasElement, anchorPoint: [number, number], rectStyler?: (ctx:CanvasRenderingContext2D) => void) {
         this.anchor = [anchorPoint[0], anchorPoint[1]];
         this.dimensions = [anchorPoint[0], anchorPoint[1], 0, 0];
         this.rootCavnas = canvas;
         this.createCanvas();
         this.positionCanvas();
         this.rectContext = this.rectCanvas.getContext("2d");
-        this.rectStyler = rectStyler || this.defaultRectStyler;
+        if (rectStyler) { rectStyler(this.rectContext); } else { this.defaultRectStyler(this.rectContext); }
     }
 
     private createCanvas() {
@@ -26,6 +25,9 @@ export class Rect {
         this.rectCanvas.width = this.rootCavnas.width;
         this.rectCanvas.height = this.rootCavnas.height;
         this.rectCanvas.style.position = "absolute";
+        this.rectCanvas.style.zIndex = ((Number(this.rootCavnas.style.zIndex)|0) || 0) + 1 + "";
+        this.rectCanvas.style.pointerEvents = "none";
+        this.rootCavnas.parentNode.appendChild(this.rectCanvas);
     }
 
     private positionCanvas() {
@@ -65,21 +67,25 @@ export class Rect {
         context.lineWidth = 2;
     }
 
+    private clearCanvas() {
+        this.rectContext.clearRect(0, 0, this.rectCanvas.width, this.rectCanvas.height);
+        this.rectContext.beginPath(); // this is needed because of using stroke() - for border
+    }
+
     getDimensions() {
         return [this.dimensions[0], this.dimensions[1], this.dimensions[2], this.dimensions[3]];
     }
 
     redraw(mousePosition: [number, number]) {
         this.recomputeRectDimensions(mousePosition);
-        this.remove();
-        this.rectStyler(this.rectContext);
+        this.clearCanvas();
         this.rectContext.rect(this.dimensions[0], this.dimensions[1], this.dimensions[2], this.dimensions[3]);
         this.rectContext.stroke();
         this.rectContext.fill();
     }
 
     remove() {
-        this.rectContext.clearRect(0, 0, this.rectCanvas.width, this.rectCanvas.height);
+        this.rectCanvas.parentNode.removeChild(this.rectCanvas);
     }
 
     // Optimisation functions
@@ -89,5 +95,5 @@ export class Rect {
     bothDimensionsDecreasing() {
         return !this.heightIncreasing && !this.widthIncreasing;
     }
-    
+
 }
