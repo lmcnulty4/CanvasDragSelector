fce34e079ae08ebf83c3b9db8a7f0021b9c19ad7
Luke McNulty <luke.mcnulty2@gmail.com>
2017-05-14 23:45:33 +0100

    Added (UNTESTED) cubic bezier and rectangle intersection

--- src/shapes/Path.ts
+++ src/shapes/Path.ts
@@ -2,13 +2,15 @@ import { IShape } from "./base";
 import { TransformMatrix } from "../ContextCurrentTransformPolyfill";
 import { Rectangle } from "./Rectangle";
 
-type Point = [number, number, "M" | "L" | "Z"]; // [x, y, type]
+type Point = [number, number, "M" | "L" | "Z"]; // x, y, type
+type QuadraticCurve = [number, number, number, number, "Q"]; // CPx, CPy, x, y, type
+type CuberBezierCurve = [number, number, number, number, number, number, "C"]; // CP1x, CP1y, CP2x, CP2y, x, y, type
 
 export class Path implements IShape {
 
     readonly boundingBox: [number, number, number, number];
     private xform: TransformMatrix;
-    private points: Point[];
+    private points: (Point | QuadraticCurve | CuberBezierCurve)[];
     private pointType: number;
 
     constructor(context: CanvasRenderingContext2D) {
@@ -30,6 +32,13 @@ export class Path implements IShape {
     }
 
     // TODO: quadraticCurveTo, bezierCurveTo, arcTo -- all done via resampling unless can find a more optimal way
+    quadraticCurveTo(controlPoint: [number, number], endPoint: [number, number]) {
+        this.points.push([controlPoint[0], controlPoint[1], endPoint[0], endPoint[1], "Q"]);
+    }
+
+    bezierCurveTo(controlPoint1: [number, number], controlPoint2: [number, number], endPoint: [number, number]) {
+        this.points.push([controlPoint1[0], controlPoint1[1], controlPoint2[0], controlPoint2[1], endPoint[0], endPoint[1], "C"]);
+    }
 
     intersects(dimens: [number, number, number, number]) {
         let transformedRect = Rectangle.matrixTransformRect(dimens, this.xform);
@@ -41,13 +50,31 @@ export class Path implements IShape {
             if (currentSegment[2] === "M") {
                 segmentInitialSubPath = currentSegment;
             } else if (currentSegment[2] === "L") {
-                if (this.lineIntersectsRect(previousSegment, currentSegment, transformedRect)) {
+                if (this.lineIntersectsRect(<Point>previousSegment, <Point>currentSegment, transformedRect)) {
                     return true;
                 }
             } else if (currentSegment[2] === "Z") {
-                if (this.lineIntersectsRect(previousSegment, segmentInitialSubPath, transformedRect)) {
+                if (this.lineIntersectsRect(<Point>previousSegment, <Point>segmentInitialSubPath, transformedRect)) {
                     return true;
                 }
+            } else if (currentSegment[4] === "Q") {
+
+            } else if (currentSegment[6] === "C") {
+                let bezierPoints = [
+                    (-previousSegment[0] + 3*currentSegment[0] + -3*(<CuberBezierCurve>currentSegment)[2] + (<CuberBezierCurve>currentSegment)[4]), // P0x
+                    (-previousSegment[1] + 3*currentSegment[1] + -3*(<CuberBezierCurve>currentSegment)[3] + (<CuberBezierCurve>currentSegment)[5]), // P0y
+                    (3*previousSegment[0] - 6*currentSegment[0] + 3*(<CuberBezierCurve>currentSegment)[2]), (3*previousSegment[1] - 6*currentSegment[1] + 3*(<CuberBezierCurve>currentSegment)[3]), // P1x, P1y
+                    (-3*previousSegment[0] + 3*currentSegment[0]), (-3*previousSegment[1] + 3*currentSegment[1]), // P2x, P2y
+                    previousSegment[0], previousSegment[1] // P3x, P3y
+                ];
+                // x, y, x+w, y
+                if (this.bezierIntersectsLineSegment(bezierPoints, [transformedRect[0], transformedRect[1], transformedRect[0] + transformedRect[2], transformedRect[1]])) return true;
+                // x+w, y, x+w, y+h
+                if (this.bezierIntersectsLineSegment(bezierPoints, [transformedRect[0] + transformedRect[2], transformedRect[1], transformedRect[0] + transformedRect[2], transformedRect[1] + transformedRect[3]])) return true;
+                // x+w, y+h, x, y+h
+                if (this.bezierIntersectsLineSegment(bezierPoints, [transformedRect[0] + transformedRect[2], transformedRect[1] + transformedRect[3], transformedRect[0], transformedRect[1] + transformedRect[3]])) return true;
+                // x, y+h, x, y
+                if (this.bezierIntersectsLineSegment(bezierPoints, [transformedRect[0], transformedRect[1] + transformedRect[3], transformedRect[0], transformedRect[1]])) return true;
             }
             previousSegment = currentSegment;
         }
@@ -91,6 +118,59 @@ export class Path implements IShape {
         v = bottomLeft[0] * yC + bottomLeft[1] * xC + os;
         if ((v < 0 && sign > 0) || (v > 0 && sign < 0)) return true;
         return false;
+    }
+
+    private bezierIntersectsLineSegment(bezierPoints: number[], lineSegment: [number, number, number, number]) {
+        let A = lineSegment[3] - lineSegment[1], B = lineSegment[0] - lineSegment[2];
+        let C = lineSegment[0]*(lineSegment[1] - lineSegment[3]) + lineSegment[1]*(lineSegment[2] - lineSegment[0]);
+        return this.hasCubicRootInSegment(
+            A*bezierPoints[0] + B*bezierPoints[1],
+            A*bezierPoints[2] + B*bezierPoints[3],
+            A*bezierPoints[4] + B*bezierPoints[5],
+            A*bezierPoints[6] + B*bezierPoints[7] + C,
+            lineSegment,
+            bezierPoints
+        );
+    }
+
+    private hasCubicRootInSegment(a: number, b: number, c: number, d: number, lineSegment: [number, number, number, number], bezierPoints: number[]): boolean {
+        let A = b/a;
+        let B = c/a;
+        let C = d/a;
+        let Q = (3*B - A*A)/9;
+        let R = (9 *A*B - 27*C - 2*A*A*A)/54;
+        let D = Q*Q*Q + R*R;
+        if (D >= 0) {
+            let S = (R + Math.sqrt(D) < 0 ? -1 : 1) * Math.pow(Math.abs(R + Math.sqrt(D)),(1/3));
+            let T = (R - Math.sqrt(D) < 0 ? -1 : 1) * Math.pow(Math.abs(R - Math.sqrt(D)),(1/3));
+            let root0 = -A/3 + (S + T);
+            if (root0 > 0 && root0 < 1 && this.withinSegment(root0, lineSegment, bezierPoints)) return true;
+            if (Math.abs(0.86602540375*(S - T)) !== 0) return false; // constant = (sqr root 3)/2 
+            let root1 = -A/3 - (S + T)/2;
+            if (root1 > 0 && root1 < 1 && this.withinSegment(root1, lineSegment, bezierPoints)) return true;
+        } else {
+            let th = Math.acos(R / Math.sqrt(-(Q*Q*Q)));
+            let coef = 2*Math.sqrt(-Q); 
+            let root0 = coef * Math.cos(th/3) - A/3;
+            if (root0 > 0 && root0 < 1 && this.withinSegment(root0, lineSegment, bezierPoints)) return true;
+            let root1 = coef * Math.cos((th + 2*Math.PI)/3) - A/3;
+            if (root1 > 0 && root1 < 1 && this.withinSegment(root1, lineSegment, bezierPoints)) return true;
+            let root2 = coef * Math.cos((th + 4*Math.PI)/3) - A/3;
+            if (root2 > 0 && root2 < 1 && this.withinSegment(root2, lineSegment, bezierPoints)) return true;
+        }
+        return false;
+    }
+
+    private withinSegment(t: number, lineSegment: [number, number, number, number], bezierPoints: number[]) {
+        let x0 = bezierPoints[0]*t*t*t + bezierPoints[2]*t*t + bezierPoints[4]*t + bezierPoints[6];
+        let x1 = bezierPoints[1]*t*t*t + bezierPoints[3]*t*t + bezierPoints[5]*t + bezierPoints[7];
+        let s: number;
+        if ((lineSegment[2] - lineSegment[0]) !== 0) {
+            s = (x0 - lineSegment[0])/(lineSegment[2] - lineSegment[0]);
+        } else {
+            s = (x1 - lineSegment[1])/(lineSegment[3] - lineSegment[1]);
+        }
+        return s > 0 && s < 1;
     }
 
 }
