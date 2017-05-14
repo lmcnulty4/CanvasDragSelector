4b942f6b79b56aca410bd43c80c4b9c003922b21
Luke McNulty <luke.mcnulty2@gmail.com>
2017-05-14 15:36:08 +0100

    Renamed class to Circle and added intersection logic

--- src/shapes/Circle.ts
+++ src/shapes/Circle.ts
@@ -1,11 +1,38 @@
-import { IShape } from "./base";
+import { IShape } from "./Base";
+import { Rectangle } from "./Rectangle";
+import { TransformMatrix } from "../ContextCurrentTransformPolyfill"
 
-export class Ellipse implements IShape {
+export class Circle implements IShape {
 
-    
+    private centre: [number, number]; // x, y
+    private radius: number;
+    private squaredRadius: number;
+    private xform: TransformMatrix;
+    readonly boundingBox: [number, number, number, number];
 
-    intersects(dimens: [number, number, number, number]) {
-        return true;
+    constructor(context: CanvasRenderingContext2D, centre: [number, number], radius: number) {
+        this.centre = [centre[0], centre[1]];
+        this.radius = radius;
+        this.squaredRadius = radius * radius;
+        this.boundingBox = [
+            centre[0] - radius,
+            centre[1] + radius,
+            radius * 2,
+            radius * 2
+        ];
+        this.xform = (<any>context).currentTransform;
+    }
+
+    intersects(rect: [number, number, number, number]) {
+        let transformedRect = Rectangle.matrixTransformRect(rect, this.xform);
+        if (!Rectangle.rectanglesIntersect(this.boundingBox, transformedRect)) return false;
+        let half = [transformedRect[2] / 2, transformedRect[3] / 2];
+        let centre = [this.centre[0] - transformedRect[0] + half[0], this.centre[1] - transformedRect[1] - half[1]];
+        let side = [Math.abs(centre[0]) - half[0], Math.abs(centre[1]) - half[1]];
+        if (side[0] > this.radius || side[1] > this.radius) return false;
+        if (side[0] < -this.radius && side[1] < -this.radius) return true;
+        if (side[0] < 0 || side[1] < 0) return true;
+        return (side[0] * side[0]) + (side[1] * side[1]) < this.squaredRadius;
     }
 
 }
