c3319a88ca12caf12243a3c2f6086c800d617da0
Luke McNulty <luke.mcnulty2@gmail.com>
2017-05-16 20:52:15 +0100

    Refactored cubic bezier functions into their own class.

--- src/shapes/Path.ts
+++ src/shapes/Path.ts
@@ -174,3 +174,132 @@ export class Path implements IShape {
     }
 
 }
+
+class CubicBezierCurve {
+
+    // These P0, P1, P2 and P3 are from the cubic x = P0(t^3) + P1(t^2) + P2(t) + P3 (and similarly for y)
+    // I've gone off arrays
+    private p0x: number;
+    private p0y: number;
+    private p1x: number;
+    private p1y: number;
+    private p2x: number;
+    private p2y: number;
+    private p3x: number;
+    private p3y: number;
+    private endX: number;
+    private endY: number;
+    private xMin: number;
+    private xMax: number;
+    private yMin: number;
+    private yMax: number;
+
+    constructor(startPoint: Point, controlPoint1: [number, number], controlPoint2: [number, number], endPoint: [number, number]) {
+        this.p0x = (-startPoint[0] + 3*controlPoint1[0] + -3*controlPoint2[0] + endPoint[0]);
+        this.p0y = (-startPoint[1] + 3*controlPoint1[1] + -3*controlPoint2[1] + endPoint[1]);
+        this.p1x = (3*startPoint[0] - 6*controlPoint1[0] + 3*controlPoint2[0]);
+        this.p1y = (3*startPoint[1] - 6*controlPoint1[1] + 3*controlPoint2[1]);
+        this.p2x = (-3*startPoint[0] + 3*controlPoint1[0]);
+        this.p2y = (-3*startPoint[1] + 3*controlPoint1[1]);
+        this.p3x = startPoint[0];
+        this.p3y = startPoint[1];
+        this.endX = endPoint[0];
+        this.endY = endPoint[1];
+        this.calculateAABB(startPoint, controlPoint1, controlPoint2, endPoint);
+    }
+
+    // Note: In 1 dimension only
+    private evaluateBezier(startPoint:number, controlPoint1: number, controlPoint2: number, endPoint: number, t: number) {
+        return startPoint * (1 - t) * (1 - t) * (1 - t) + 3 * controlPoint1 * t * (1 - t) * (1 - t) + 3 * controlPoint2 * t * t * (1 - t) + endPoint * t * t * t;
+    }
+
+    // https://pomax.github.io/bezierinfo/#boundingbox
+    // https://pomax.github.io/bezierinfo/#extremities
+    private calculateAABB(startPoint: Point, controlPoint1: [number, number], controlPoint2: [number, number], endPoint: [number, number]) {
+        // Derivative and get roots
+        let aX = 3 * this.p0x, aY = 3 * this.p0y;
+        let bX = 2 * this.p1x, bY = 2 * this.p1y;
+        let cX = this.p2x, cY = this.p2y;
+        // initialise mins and maxes
+        this.xMin = endPoint[0] < startPoint[0] ? endPoint[0] : startPoint[0], this.xMax = endPoint[0] > startPoint[0] ? endPoint[0] : startPoint[0];
+        this.yMin = endPoint[1] < startPoint[1] ? endPoint[1] : startPoint[1], this.yMax = endPoint[1] > startPoint[1] ? endPoint[1] : startPoint[1];
+        // roots where (-b +- sqrt(b*b - 4*a*c)) / 2*a = 0
+        let discX = bX * bX - 4 * aX * cX, discY = bY * bY - 4 * aY * cY;
+        if (discX >= 0) {
+            let t1 = (-bX + Math.sqrt(discX)) / (2*aX), t2 = (-bX - Math.sqrt(discX)) / (2*aX); // from 
+            if (t1 > 0 && t1 < 1) {
+                let xt = this.evaluateBezier(startPoint[0], controlPoint1[0], controlPoint2[0], endPoint[0], t1);
+                if (xt < this.xMin) this.xMin = xt;
+                if (xt > this.xMax) this.xMax = xt;
+            }
+            if (t2 > 0 && t2 < 1) {
+                let xt = this.evaluateBezier(startPoint[0], controlPoint1[0], controlPoint2[0], endPoint[0], t2);
+                if (xt < this.xMin) this.xMin = xt;
+                if (xt > this.xMax) this.xMax = xt;
+            }
+        }
+        if (discY >= 0) {
+            let t1 = (-bY + Math.sqrt(discY)) / (2*aY), t2 = (-bY - Math.sqrt(discY)) / (2*aY);
+            if (t1 > 0 && t1 < 1) {
+                let xt = this.evaluateBezier(startPoint[1], controlPoint1[1], controlPoint2[1], endPoint[1], t1);
+                if (xt < this.yMin) this.yMin = xt;
+                if (xt > this.yMax) this.yMax = xt;
+            }
+            if (t2 > 0 && t2 < 1) {
+                let xt = this.evaluateBezier(startPoint[1], controlPoint1[1], controlPoint2[1], endPoint[1], t2);
+                if (xt < this.yMin) this.yMin = xt;
+                if (xt > this.yMax) this.yMax = xt;
+            }
+        }
+    }
+
+    intersects(rect: [number, number, number, number]): boolean {
+        // If either start or end point is inside rect, it intersects
+        if (rect[0] <= this.p3x && rect[0] + rect[2] >= this.p3x && rect[1] <= this.p3y && rect[1] + rect[3] >= this.p3y) return true;
+        if (rect[0] <= this.endX && rect[0] + rect[2] >= this.endX && rect[1] <= this.endY && rect[1] + rect[3] >= this.endY) return true;
+        // If it doesn't intersect bounding box, exit
+        if (!Rectangle.rectanglesIntersect(rect, [this.xMin, this.yMin, this.xMax - this.xMin, this.yMax - this.yMin])) return false;
+        return true;
+    }
+
+    // https://www.particleincell.com/2013/cubic-line-intersection/
+    private hasCubicRootInSegment(a: number, b: number, c: number, d: number, lineSegment: [number, number, number, number], bezierPoints: number[]): boolean {
+        let A = b/a;
+        let B = c/a;
+        let C = d/a;
+        let Q = (3*B - A*A)/9;
+        let R = (9 *A*B - 27*C - 2*A*A*A)/54;
+        let D = Q*Q*Q + R*R;
+        if (D >= 0) {
+            let S = (R + Math.sqrt(D) < 0 ? -1 : 1) * Math.pow(Math.abs(R + Math.sqrt(D)),(1/3));
+            let T = (R - Math.sqrt(D) < 0 ? -1 : 1) * Math.pow(Math.abs(R - Math.sqrt(D)),(1/3));
+            let root0 = -A/3 + (S + T);
+            if (root0 > 0 && root0 < 1 && this.withinSegment(root0, lineSegment, bezierPoints)) return true;
+            if (Math.abs(0.86602540375*(S - T)) !== 0) return false; // constant = (sqr root 3)/2 
+            let root1 = -A/3 - (S + T)/2;
+            if (root1 > 0 && root1 < 1 && this.withinSegment(root1, lineSegment, bezierPoints)) return true;
+        } else {
+            let th = Math.acos(R / Math.sqrt(-(Q*Q*Q)));
+            let coef = 2*Math.sqrt(-Q); 
+            let root0 = coef * Math.cos(th/3) - A/3;
+            if (root0 > 0 && root0 < 1 && this.withinSegment(root0, lineSegment, bezierPoints)) return true;
+            let root1 = coef * Math.cos((th + 2*Math.PI)/3) - A/3;
+            if (root1 > 0 && root1 < 1 && this.withinSegment(root1, lineSegment, bezierPoints)) return true;
+            let root2 = coef * Math.cos((th + 4*Math.PI)/3) - A/3;
+            if (root2 > 0 && root2 < 1 && this.withinSegment(root2, lineSegment, bezierPoints)) return true;
+        }
+        return false;
+    }
+
+    private withinSegment(t: number, lineSegment: [number, number, number, number], bezierPoints: number[]) {
+        let x0 = bezierPoints[0]*t*t*t + bezierPoints[2]*t*t + bezierPoints[4]*t + bezierPoints[6];
+        let x1 = bezierPoints[1]*t*t*t + bezierPoints[3]*t*t + bezierPoints[5]*t + bezierPoints[7];
+        let s: number;
+        if ((lineSegment[2] - lineSegment[0]) !== 0) {
+            s = (x0 - lineSegment[0])/(lineSegment[2] - lineSegment[0]);
+        } else {
+            s = (x1 - lineSegment[1])/(lineSegment[3] - lineSegment[1]);
+        }
+        return s > 0 && s < 1;
+    }
+}
